3b00387828e52adef1ce7dd0b70516644622d99b
Hawk Itzme <smquasim016@gmail.com>
2025-04-09 19:45:59 +0530

    URls updated

--- app/src/main/java/com/largemlmodelencryption/utility/Model_Decryptor.kt
+++ app/src/main/java/com/largemlmodelencryption/utility/Model_Decryptor.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.util.Log
 import java.io.ByteArrayInputStream
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.io.IOException
 import javax.crypto.Cipher
 import javax.crypto.CipherInputStream
 import javax.crypto.spec.GCMParameterSpec
@@ -15,7 +17,7 @@ object Model_Decryptor_KeyStore {
     private const val TAG_SIZE = 16 // 128-bit authentication tag
     private const val BUFFER_SIZE = 1024 * 4 // 4KB buffer
 
-    suspend fun decryptModel(context: Context, encryptedFileName: String): File {
+   /* suspend fun decryptModel(context: Context, encryptedFileName: String): File {
         val encryptedFile = File(context.getExternalFilesDir(null), encryptedFileName)
         val outputFile = File(context.getExternalFilesDir(null), "decrypted_model.tflite")
         val secretKey = AESKeyManager.getDecryptedAESKey(context)
@@ -38,6 +40,34 @@ object Model_Decryptor_KeyStore {
             CipherInputStream(byteStream, cipher).use { cis ->
                 FileOutputStream(outputFile).use { fos ->
                    // val buffer = ByteArray(8192)
+                    val buffer = ByteArray(BUFFER_SIZE)
+                    var bytesRead: Int
+                    while (cis.read(buffer).also { bytesRead = it } != -1) {
+                        fos.write(buffer, 0, bytesRead)
+                    }
+                }
+            }
+        }
+        return outputFile
+    }*/
+
+        suspend fun decryptModel(context: Context, encryptedFileName: String): File {
+        val encryptedFile = File(context.getExternalFilesDir(null), encryptedFileName)
+        val outputFile = File(context.getExternalFilesDir(null), "decrypted_model.tflite")
+        var secretKey = AESKeyManager.getDecryptedAESKey(context)
+
+        FileInputStream(encryptedFile).use { fis ->
+            val iv = ByteArray(IV_SIZE)
+            if (fis.read(iv) != IV_SIZE) {
+                throw IOException("Unable to read IV from encrypted file.")
+            }
+
+            val cipher = Cipher.getInstance(TRANSFORMATION)
+            val spec = GCMParameterSpec(TAG_SIZE * 8, iv)
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
+
+            CipherInputStream(fis, cipher).use { cis ->
+                FileOutputStream(outputFile).use { fos ->
                     val buffer = ByteArray(BUFFER_SIZE)
                     var bytesRead: Int
                     while (cis.read(buffer).also { bytesRead = it } != -1) {
